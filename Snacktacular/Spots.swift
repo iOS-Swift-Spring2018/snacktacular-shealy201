3db6442cd10fce2a91680c65f1ff9567c9387361
Sean Healy <seanhealy@seans-macbook-air.local>
2018-04-19 12:59:20 -0400

    Initial Commit

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,35 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Sean Healy on 4/18/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Spots {
+    var spotArray = [Spot]()
+    var db: Firestore!
+    
+    init() {
+        db = Firestore.firestore()
+    }
+    
+    func loadData(completed: @escaping () -> ()) {
+        db.collection("spots").addSnapshotListener { (querySnapshot, error) in
+            guard error == nil else {
+                print("*** ERROR: adding the snapshot listener \(error!.localizedDescription)")
+                return completed()
+            }
+            self.spotArray = []
+            for document in querySnapshot!.documents {
+                let spot = Spot(dictionary: document.data())
+                spot.documentID = document.documentID
+                self.spotArray.append(spot)
+            }
+            completed()
+        }
+    }
+}
